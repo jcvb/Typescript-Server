7fada06e70f387172b74b87b022a7b5462281c94
Juan Carlos <jvalerio@reprintsdesk.com>
2023-10-02 19:43:18 -0600

    fix(API): :bug: cors

--- dist/src/app.js
+++ dist/src/app.js
@@ -6,6 +6,8 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const posts_routes_1 = __importDefault(require("../routes/posts.routes"));
 const app = (0, express_1.default)();
+const cors = require("cors");
+app.use(cors());
 app.use(express_1.default.json());
 app.use(posts_routes_1.default);
 exports.default = app;
--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,9 @@
-import express, { Express } from 'express';
-import postsRoutes from '../routes/posts.routes';
+import express, { Express } from "express";
+import postsRoutes from "../routes/posts.routes";
 const app: Express = express();
+const cors = require("cors");
 
+app.use(cors());
 app.use(express.json());
 
 app.use(postsRoutes);
