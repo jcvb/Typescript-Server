6bbc411db5810c90c782d7e01dd82c54cbba1a38
Juan Carlos <jvalerio@reprintsdesk.com>
2023-10-02 18:53:56 -0600

    fix(API): :bug: npm

--- database/database.ts
+++ database/database.ts
@@ -3,6 +3,7 @@ import { Sequelize } from "sequelize";
 export const sequelize = new Sequelize("git-posts", "fl0user", "as8nNjqdK6Vc", {
   host: "ep-empty-salad-13069354.ap-southeast-1.aws.neon.fl0.io",
   dialect: "postgres",
+  dialectModule: require('pg'),
   dialectOptions: {
     ssl: true,
   },
